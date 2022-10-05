d81be34f653d388a6c616a1ac73eb846a73e9782
nelsonlai <nelsonlai9503@gmail.com>
2022-10-05 14:23:56 +0800

    refactor naming

--- sys_config/register.go
+++ sys_config/register.go
@@ -10,12 +10,12 @@ const PluginID = "27eb1542-ce62-4e6c-b284-fbda8c6bca2d"
 var db *gorm.DB
 var mem *gorm.DB
 
-type Config struct {
+type Option struct {
 	DB  *gorm.DB
 	MEM *gorm.DB
 }
 
-func RegisterHandler(e *ginseng.Engine, option *Config) {
+func RegisterHandler(e *ginseng.Engine, option *Option) {
 	db = option.DB
 	mem = option.MEM
 	db.AutoMigrate(&SystemConfig{})
